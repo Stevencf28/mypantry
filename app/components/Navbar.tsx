ae2e0240164c83d54139fc1eb67bffe7099288c7
Steven Chen <steven.cf28@gmail.com>
2025-03-20 19:26:04 -0400

    added dark mode functionality

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/16/solid";
+import { Switch } from "@headlessui/react";
+import { Bars3Icon } from "@heroicons/react/16/solid";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { ThemeContext } from "../context/Contexts";
 
 interface NavbarProps {
 	children: React.ReactNode;
@@ -15,65 +17,122 @@ const links = [
 	{ name: "My Recipes", href: "/recipes" },
 ];
 
+//change theme function
+
 export default function Navbar({ children }: NavbarProps) {
 	const [open, setOpen] = useState(false);
+	const [theme, setTheme] = useState("");
+
+	const toggleTheme = () => {
+		const newTheme = theme === "light" ? "dark" : "light";
+		localStorage.setItem("theme", newTheme);
+		setTheme(newTheme);
+	};
+
+	useEffect(() => {
+		const localTheme = localStorage.getItem("theme");
+		setTheme(localTheme || "light");
+		console.log("theme: ", theme);
+	}, []);
+
 	return (
 		<>
-			{/* Top nav */}
-			<nav className="bg-[url(/img/wood.webp)] h-14 border-b-2">
-				<div className="flex flex-row px-6 w-full h-full justify-between items-center">
-					{/* left side */}
-					<div className="flex flex-row gap-x-4 w-full">
-						<button
-							onClick={() => setOpen((open) => !open)}
-							className="cursor-pointer"
-						>
-							<Bars3Icon className="size-8" />
-						</button>
-						<Link href={"/"}>
-							<h2>My Pantry</h2>
-						</Link>
-					</div>
-					{/* right side */}
-					<div className="flex flex-row gap-x-4">
-						<Link className="hover:bg-gray-600 rounded-lg" href={"/login"}>
-							Login
-						</Link>
-					</div>
-				</div>
-			</nav>
-			<div className="flex flex-row">
-				{/* Side nav */}
-				<div
-					className={`bg-[url(/img/wood.webp)] h-[calc(100vh-3.5rem)] relative transition-all duration-200 pt-4 ${
-						open ? "w-40" : "w-0"
-					}`}
-				>
-					<div
-						className={`flex flex-col justify-evenly mx-2 relative transition-all duration-200 ${
-							open ? "translate-x-0" : "translate-x-[-140px]"
-						}`}
+			<ThemeContext.Provider value={theme}>
+				<div id="nav">
+					{/* Top nav */}
+					<nav
+						className={` h-14 border-b-2 ${
+							theme === "dark"
+								? "dark bg-[url(/img/dark.png)]"
+								: "light bg-[url(/img/light.webp)]"
+						}
+					transition-all duration-200
+					`}
 					>
-						{links.map((link) => (
-							<button key={link.href} className="mb-2 border-b">
-								<Link href={link.href}>
-									<h2 className="text-start text-clip overflow-hidden">
-										{link.name}
-									</h2>
+						<div className="flex flex-row px-6 w-full h-full justify-between items-center">
+							{/* left side */}
+							<div className="flex flex-row gap-x-4 w-full">
+								<button
+									onClick={() => setOpen((open) => !open)}
+									className="cursor-pointer hover:bg-black/40 rounded-lg p-2"
+								>
+									<Bars3Icon className="size-8 transition-all duration-200" />
+								</button>
+								<button className="hover:bg-black/40 rounded-lg p-2">
+									<Link
+										href={"/"}
+										className="text-xl transition-all duration-200"
+									>
+										My Pantry
+									</Link>
+								</button>
+							</div>
+							{/* right side */}
+							<div className="flex flex-row gap-x-4">
+								<Link
+									className="hover:bg-black/40 rounded-lg p-2 text-xl transition-all duration-200"
+									href={"/login"}
+								>
+									Login
 								</Link>
-							</button>
-						))}
+							</div>
+						</div>
+					</nav>
+					<div className="flex flex-row">
+						{/* Side nav */}
+						<div
+							className={`x h-[calc(100vh-3.5rem)] relative transition-all duration-200   
+							${open ? "w-40" : "w-0"}
+							${
+								theme === "dark"
+									? "dark bg-[url(/img/dark.png)]"
+									: "light bg-[url(/img/light.webp)]"
+							}
+							`}
+						>
+							<div
+								className={` pt-4 flex flex-col justify-evenly relative transition-all duration-200 ${
+									open ? "translate-x-0" : "translate-x-[-145px]"
+								}`}
+							>
+								{links.map((link) => (
+									<div className="mb-2 w-full " key={link.href}>
+										<button className="w-40">
+											<Link href={link.href}>
+												<h2 className="overflow-hidden text-center hover:bg-black/40 rounded-lg p-2">
+													{link.name}
+												</h2>
+											</Link>
+										</button>
+									</div>
+								))}
+								{/* dark mode switch */}
+								<div className="flex flex-row w-full justify-center items-center">
+									<Switch
+										checked={theme === "dark"}
+										onChange={() => {
+											toggleTheme();
+										}}
+										className="group inline-flex h-6 w-11 items-center rounded-full bg-gray-200 transition data-[checked]:bg-black cursor-pointer mr-2"
+									>
+										<span className="size-4 translate-x-1 rounded-full bg-white transition group-data-[checked]:translate-x-6" />
+									</Switch>
+									<h3 className="font-semibold">Dark Mode</h3>
+								</div>
+							</div>
+						</div>
+						<div
+							id="children"
+							className={`relative w-full transition-all duration-200 
+							${open ? "w-[calc(100%-10rem)]" : "w-full"}
+							${theme === "dark" ? "dark" : "light"}
+						`}
+						>
+							{children}
+						</div>
 					</div>
 				</div>
-				<div
-					id="children"
-					className={`relative w-full transition-all duration-200 ${
-						open ? "w-[calc(100%-10rem)]" : "w-full"
-					}`}
-				>
-					{children}
-				</div>
-			</div>
+			</ThemeContext.Provider>
 		</>
 	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
 	return (
-		<section className="">
+		<section className="bg-white">
 			{/* background video */}
 			<div className="absolute inset-0 w-full -z-10">
 				<div className="absolute inset-0 bg-black/60 z-10" />
@@ -15,9 +15,7 @@ export default function Home() {
 				</video>
 			</div>
 			{/* content */}
-			<div className="relative ">
-				<h1 className="font-bold">Home</h1>
-			</div>
+			<div className="relative "></div>
 		</section>
 	);
 }
